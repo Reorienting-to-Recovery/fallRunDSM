cf3e367a13a4ac38a24a56aeb63189e4fddfa86b
Erin Cain <ecain@flowwest.com>
2023-02-06 17:20:15 -0800

    Work on visuals for preformance metrics

--- data-raw/explore-results.R
+++ data-raw/explore-results.R
@@ -1,77 +1,60 @@
 library(tidyverse)
-
+library(fallRunDSM)
+library(plotly)
+# Source helper functions
+source("data-raw/helper_graph_functions.R")
 # View prelim R2R results
 set.seed(123119)
 r2r_seeds <- fallRunDSM::fall_run_model(mode = "seed", ..params = fallRunDSM::r_to_r_baseline_params)
 
 r2r_model_results <- fallRunDSM::fall_run_model(mode = "simulate", ..params = fallRunDSM::r_to_r_baseline_params,
                                                 seeds = r2r_seeds)
+# BASIC ABUNDANCE PLOTS
+# Total natural spawners plot --------------------------------------------------
+plot_total_natural_spawners(r2r_model_results, "Total Natural Spawners")
 
-r22_baseline_nat_spawn <- dplyr::as_tibble(r2r_model_results$spawners * r2r_model_results$proportion_natural) |>
-  dplyr::mutate(location = fallRunDSM::watershed_labels)
+# Total Spawners (natural and hatchery )
+plot_total_natural_spawners(r2r_model_results, "Total Spawners")
 
-# Total natural spawners plot
-r22_baseline_nat_spawn %>%
-  pivot_longer(cols = c(`1`:`20`), values_to = 'natural_spawners', names_to = "year") %>%
-  group_by(year) |>
-  summarize(total_spawners = sum(natural_spawners)) |>
-  mutate(year = as.numeric(year)) %>%
-  ggplot(aes(year, total_spawners)) +
-  geom_line() +
-  theme_minimal() +
-  labs(y = "Total Natural Spawners",
-       x = "Year") +
-  scale_y_continuous(labels = scales::comma) +
-  scale_x_continuous(breaks = 1:20) +
-  theme(text = element_text(size = 20))
+# Single watershed total spawners
+plot_single_watershed_natural_spawners(model_results = r2r_model_results,
+                                       watershed = "Battle Creek",
+                                       result_type = "Total Spawners")
 
-# CCR plot
-natural_spawners <- (r2r_model_results$proportion_natural * r2r_model_results$spawners) |>
-  as_tibble() |>
-  mutate(watershed = fallRunDSM::watershed_labels) |>
-  pivot_longer(names_to = "year", values_to = "natural_spawners", -watershed)
+# All watershed
+plot_all_watersheds_spawners(model_results = r2r_model_results,
+                             result_type = "Total Spawners")
+plot_all_watersheds_spawners(model_results = r2r_model_results,
+                             result_type = "Total Natural Spawners")
+# CCR Results ------------------------------------------------------------------
+# Adult to adult metric --------------------------------------------------------
+# Data processing
+# TODO these show an example where we just plot the mean - the adult to adult plots
+# show an example where we plot each watershed. Confirm which route we want to go?
+plot_juv_crr(model_results = r2r_model_results,
+               result_type = "Total Spawners")
 
-juveniles <- r2r_model_results$juveniles |>
-  as_tibble() |>
-  mutate(year = as.numeric(year)) |>
-  group_by(year, watershed) |>
-  summarise(total_juveniles = sum(juveniles)) |>
-  ungroup() |>
-  left_join(natural_spawners |>
-              mutate(year = as.numeric(year))) |>
-  arrange(watershed, year) |>
-  group_by(watershed) |>
-  mutate(natural_spawners_lag = lead(natural_spawners, 3),
-         metric = total_juveniles / natural_spawners_lag,
-         metric_rev = natural_spawners_lag / total_juveniles) |>
-  ungroup()
+plot_juv_crr(model_results = r2r_model_results,
+             result_type = "Total Natural Spawners")
+## CCR adults to adults --------------------------------------------------------
+# data processing
+# TODO confirm we do not want total spawn and natural spawn compared against each other
+plot_adult_crr(model_results = r2r_model_results,
+               result_type = "Total Spawners")
 
-juv_plot <- juveniles |>
-  group_by(year) |>
-  summarize(mean_crr = mean(metric_rev, na.rm = T)) |>
-  ggplot(aes( year, mean_crr)) +
-  geom_line() +
-  scale_x_continuous(breaks = 1:20) +
-  ylab('Adult Returns Per Juveniles') +
-  xlab("Year") +
-  theme_minimal() +
-  theme(text = element_text(size = 20))
+plot_adult_crr(model_results = r2r_model_results,
+               result_type = "Total Natural Spawners")
 
-juv_plot
+# Habitat percentages by watershed ---------------------------------------------
+produce_habitat_ratios(model_parameters = r_to_r_baseline_params, watershed = "Yuba River")
+produce_habitat_ratios(model_parameters = r_to_r_baseline_params, watershed = "Lower Sacramento River")
 
+
+### OG MODEL COMPARISON ########################################################
 # OG model results
 set.seed(123119)
 seeds <- fallRunDSM::fall_run_model(mode = "seed", ..params = fallRunDSM::params)
 
 model_results <- fallRunDSM::fall_run_model(mode = "simulate", ..params = fallRunDSM::params, seeds = seeds)
 
-nat_spawn <- dplyr::as_tibble(model_results$spawners * model_results$proportion_natural) |>
-  dplyr::mutate(location = fallRunDSM::watershed_labels)
-
-nat_spawn %>%
-  pivot_longer(cols = c(`1`:`20`), values_to = 'natural_spawners', names_to = "year") %>%
-  mutate(year = as.numeric(year)) %>%
-  ggplot(aes(year, natural_spawners, color = location)) +
-  geom_line() +
-  theme_minimal()
-
+plot_total_natural_spawners(model_results, "Total Natural Spawners")

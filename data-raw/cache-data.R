c04ced23453d5de6c8f9584cb52c44f8c8e37b20
Erin Cain <ecain@flowwest.com>
2023-06-30 09:41:21 -0700

    Adds comment to cache data to refer to location of R2R data objects

--- data-raw/cache-data.R
+++ data-raw/cache-data.R
@@ -129,49 +129,8 @@ watershed_attributes <- read_csv("data-raw/watershed-attributes.csv")
 usethis::use_data(watershed_attributes, overwrite = TRUE)
 
 # R2R cache data objects
-# hatchery releases in river
-fall_releases <- readxl::read_excel(path = "data-raw/analysis/hatchery_release_analysis/CVFRChin_RELEASE DB_v3_POSTED030419 (1).xlsx", sheet = "DBTable1") |> glimpse()
-summarized_fall_release <- fall_releases |>
-  filter(Release_year >= 1995) |>
-  group_by(Release_year, Hatchery) |>
-  summarise(total_release = sum(Total_N, na.rm = TRUE)) |>
-  group_by(Hatchery) |>
-  summarize(seventy_fifth_percentile = quantile(total_release, .75),
-            max_release = max(total_release, na.rm = TRUE),
-            median_release = median(total_release, na.rm = TRUE)) |>
-  glimpse()
+# see hatchery markdown/analysis and harvest analysis for data objects
+# TODO eventually clean up and move into this script
 
-
-summarized_fall_release$Hatchery |> unique()
-summarized_fall_release$Release_site |> unique()
-
-pairs <- matrix(c("COL", "Coleman National Fish Hatchery", "Battle Creek",
-                  "FEA", "Feather River Hatchery", "Feather River",
-                  "MER", "Merced River Fish Facility", "Merced River",
-                  "NIM", "Nimbus Fish Hatchery", "American River",
-                  "MOK", "Mokelumne Hatchery", "Mokelumne River"),
-                ncol=3,
-                byrow=T,
-                dimnames = list(c(), c("Hatchery", "hatchery_name", "river"))) |>
-  as_tibble(.name_repair = "check_unique") |> glimpse()
-
-summarized_release <- left_join(summarized_fall_release, pairs) |>
-  select(watershed = river, release_number = seventy_fifth_percentile) |>
-  right_join(watershed_attributes |> select(watershed, order)) |>
-  arrange(order) |>
-  mutate(release_number = ifelse(is.na(release_number), 0, release_number)) |>
-  glimpse()
-
-# use values to populate named vector
-baseline_fall_hatchery_release = matrix(0, nrow = 31, ncol = 4, dimnames = list(fallRunDSM::watershed_labels, c("s", "m", "l", "xl")))
-
-# populate hatchery release as larger fish based on avg hatchery release data
-baseline_fall_hatchery_release[, 3] <- summarized_release$release_number
-
-usethis::use_data(baseline_fall_hatchery_release, overwrite = TRUE)
-
-# hatchery releases at chipps
-baseline_hatchery_releases_at_chipps <- matrix(0, nrow = 31, ncol = 4, dimnames = list(fallRunDSM::watershed_labels, fallRunDSM::size_class_labels))
-usethis::use_data(baseline_hatchery_releases_at_chipps, overwrite = TRUE)
 
 
